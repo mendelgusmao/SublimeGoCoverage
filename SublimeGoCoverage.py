dd602575d95e97eb4a2d9ec4afb26d69c5111076
MendelGusmao <mendelsongusmao@gmail.com>
2014-09-30 19:25:54 -0300

    Configurable test executor

--- SublimeGoCoverage.py
+++ SublimeGoCoverage.py
@@ -3,9 +3,11 @@ import sublime_plugin
 import subprocess
 import os
 import re
+import shlex
 
 gopath = os.environ["GOPATH"]
 line_re = re.compile("^(?P<filename>.+):(?P<start_line>[0-9]+).(?P<start_column>[0-9]+),(?P<end_line>[0-9]+).(?P<end_column>[0-9]+) (?P<statements>[0-9]+) (?P<count>[0-9]+)$")
+settings = "SublimeGoCoverage.sublime-settings"
 
 class ShowGoCoverageCommand(sublime_plugin.TextCommand):
 	"""Run gocov and highlight uncovered lines in the current file."""
@@ -19,11 +21,22 @@ class ShowGoCoverageCommand(sublime_plugin.TextCommand):
 
 		package_dir = os.path.dirname(filename)
 		package_name = os.path.basename(package_dir)
-
-		print("Generating coverage profile for", package_dir.replace(gopath + "/src/", ""))
-		
-		result = subprocess.check_output([gopath + "/bin/ginkgo", "-cover", "-race", package_dir])
+		package_full_name = package_dir.replace(gopath + "/src/", "")
 		coverprofile = "%s/%s.coverprofile" % (package_dir, package_name)
+
+		print("Generating coverage profile for", package_dir)
+		
+		cov = sublime.load_settings(settings).get("cov")
+		add_args = sublime.load_settings(settings).get("add_args") or ""
+
+		cmd = ""
+
+		if cov == "go-test":
+			cmd = "go test %s -cover -coverprofile=%s %s" % (add_args, coverprofile, package_full_name)
+		elif cov == "ginkgo":
+			cmd = "ginkgo %s -cover %s" % (add_args, package_dir)
+
+		result = subprocess.check_output(shlex.split(cmd))
 
 		view.erase_status("SublimeGoCoverage")
 		view.erase_regions("SublimeGoCoverage")
