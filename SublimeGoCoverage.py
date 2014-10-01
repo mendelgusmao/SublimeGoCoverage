bdbcc4834d11ea7952cd8d933ffdc8ab316547ec
MendelGusmao <mendelsongusmao@gmail.com>
2014-10-01 15:34:07 -0300

    Show coverage after saving file

--- SublimeGoCoverage.py
+++ SublimeGoCoverage.py
@@ -9,6 +9,12 @@ gopath = os.environ["GOPATH"]
 line_re = re.compile("^(?P<filename>.+):(?P<start_line>[0-9]+).(?P<start_column>[0-9]+),(?P<end_line>[0-9]+).(?P<end_column>[0-9]+) (?P<statements>[0-9]+) (?P<count>[0-9]+)$")
 settings = "SublimeGoCoverage.sublime-settings"
 
+class ShowGoCoverageListener(sublime_plugin.EventListener):
+	"""Event listener to highlight uncovered lines when a Go file is loaded."""
+
+	def on_post_save_async(self, view):
+		view.run_command('show_go_coverage')
+
 class ShowGoCoverageCommand(sublime_plugin.TextCommand):
 	"""Run gocov and highlight uncovered lines in the current file."""
 
@@ -32,7 +38,7 @@ def run_coverage(filename):
 	package_full_name = package_dir.replace(gopath + "/src/", "")
 	coverprofile = "%s/%s.coverprofile" % (package_dir, package_name)
 
-	print("Generating coverage profile for", package_dir)
+	print("Generating coverage profile for", package_full_name)
 	
 	runner = sublime.load_settings(settings).get("runner") or "go-test"
 	moreargs = sublime.load_settings(settings).get("moreargs") or ""
